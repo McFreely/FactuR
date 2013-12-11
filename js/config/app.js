59ec188707682898383dcfce94a6d177cab3cd65
McFreely <paulmcfreely@gmail.com>
2013-12-11 14:13:07 +0100

    Adds basic styling with zurb foundation

--- js/config/app.js
+++ js/config/app.js
@@ -1,9 +1,9 @@
 // require other, dependencies here, ie:
 // require('./vendor/moment');
 
-require('../vendor/jquery');
-require('../vendor/handlebars');
-require('../vendor/ember');
+// require('../vendor/jquery');
+// require('../vendor/handlebars');
+// require('../vendor/ember');
 require('../vendor/ember-data'); // delete if you don't want ember-data
 
 var App = Ember.Application.create();
